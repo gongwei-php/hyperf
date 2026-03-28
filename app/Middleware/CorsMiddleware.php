d7d14030c8d1c7f1c65fd54aa093198e37f3179e
gongwei-php <1178397506@qq.com>
2026-03-28 22:47:33 +0800

    1

---
+++ app/Middleware/CorsMiddleware.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Middleware;
+
+use Hyperf\Context\Context;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+class CorsMiddleware implements MiddlewareInterface
+{
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        $response = Context::get(ResponseInterface::class);
+        $response = $response->withHeader('Access-Control-Allow-Origin', '*')
+            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+            ->withHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+        // 处理OPTIONS预检请求
+        if (strtoupper($request->getMethod()) === 'OPTIONS') {
+            return $response->withStatus(204);
+        }
+
+        return $handler->handle($request);
+    }
+}
